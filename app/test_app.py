5fd3692961c269a9e092895abb5143d2924d41ca
agrashish <agrawalaashish1@gmail.com>
2020-03-30 22:30:44 -0500

    added basic test cases

--- app/test_app.py
+++ app/test_app.py
@@ -20,18 +20,21 @@ class FlaskAppTests(unittest.TestCase):
     def tearDown(self):
         pass
 
-    def test_home_status_code(self):
+    def test_index_unauth(self):
         # sends HTTP GET request to the application
         # on the specified path
         result = self.app.get('/')
-
-        # assert the status code of the response
-        self.assertEqual(result.status_code, 200)
-
-    def test_home_data(self):
-        # sends HTTP GET request to the application
-        # on the specified path
-        result = self.app.get('/')
-
+        exists = result.data.find("Welcome to the UIC School Notifier, please log in or register.".encode()) != -1
         # assert the response data
-        self.assertEqual(result.data, "Welcome to the UIC School Notifier, please log in or register.")
+        self.assertEqual(exists, True)
+    
+    def test_login_unauth(self):
+        result = self.app.get('/login')
+        exists = result.data.find("Sign In".encode()) != -1
+        self.assertEqual(exists, True)
+
+    def test_register_unauth(self):
+        result = self.app.get('/register')
+        exists = result.data.find("Register".encode()) != -1
+        self.assertEqual(exists, True)
+    
