ff03dd155175c22796b49f3dabffc5a5efab0cfa
agrashish <agrawalaashish1@gmail.com>
2020-03-30 23:48:40 -0500

    finished test cases for checkpoint 3

--- app/test_app.py
+++ app/test_app.py
@@ -1,6 +1,8 @@
 from app import app
 import unittest
 
+# got it from the source mentioned in sources.txt
+
 class FlaskAppTests(unittest.TestCase):
 
     @classmethod
@@ -20,44 +22,65 @@ class FlaskAppTests(unittest.TestCase):
     def tearDown(self):
         pass
 
+    # check to see if the index page loads the correct message when we aren't logged in
     def test_index_unauth(self):
-        # sends HTTP GET request to the application
-        # on the specified path
         result = self.app.get('/')
         exists = result.data.find("Welcome to the UIC School Notifier, please log in or register.".encode()) != -1
-        # assert the response data
         self.assertEqual(exists, True)
     
+    # check to see if the login page loads the correct message when we aren't logged in
     def test_login_unauth(self):
         result = self.app.get('/login')
         exists = result.data.find("Sign In".encode()) != -1
         self.assertEqual(exists, True)
 
+    # check to see if the register page loads the correct message when we aren't logged in
     def test_register_unauth(self):
         result = self.app.get('/register')
         exists = result.data.find("Register".encode()) != -1
         self.assertEqual(exists, True)
 
+    # check to see if we can register properly
     def test_register_auth(self):
-        result = self.app.post('/register', data = {"username" : "agf", "password" : "agf"}, follow_redirects=True)
+        result = self.app.post('/register', data = {"username" : "m", "password" : "agfgggg"}, follow_redirects=True)
         exists = result.data.find("Sign In".encode()) != -1
         self.assertEqual(exists, True)
 
-    def test_login_auth(self):
-        result = self.app.post('/login', data = {"username" : "agf", "password" : "agf"}, follow_redirects=True)
-        exists = result.data.find("Welcome to the UIC School Notifier, agf".encode()) != -1
+    #check to see if we can register, then log in properly
+    def test_register_login_auth(self):
+        result = self.app.post('/register', data = {"username" : "l", "password" : "agfgggg"}, follow_redirects=True)
+        exists = result.data.find("Sign In".encode()) != -1
+        result = self.app.post('/login', data = {"username" : "l", "password" : "agfgggg"}, follow_redirects=True)
+        exists = result.data.find("Welcome to the UIC School Notifier, l".encode()) != -1
         self.assertEqual(exists, True)
 
+    # check to make sure that you can't register the same username twice
+    def test_double_register_auth(self):
+        result = self.app.post('/register', data = {"username" : "x", "password" : "agfgghhh"}, follow_redirects=True)
+        exists = result.data.find("Sign In".encode()) != -1
+        result = self.app.post('/register', data = {"username" : "x", "password" : "agfgghhh"}, follow_redirects=True)
+        exists = result.data.find("Username already exists".encode()) != -1
+        self.assertEqual(exists, True)
+
+
+    # check to see if we can update our piazza credentials properly
     def test_register_piazza_auth(self):
         pass
 
+    # check to see if the contents of our piazza account are loaded properly
     def test_contents_piazza_auth(self):
         pass
 
+    # check to see if we can update our gradescope credentials properly
     def test_register_gradescope_auth(self):
         pass
 
+    # check to see if the contents of our gradescope account are loaded properly
     def test_contents_gradescope_auth(self):
+        pass
+
+    # check if list of classes loads properly for gradescope, current semester
+    def test_className_gradescope_auth(self):
         pass
 
     
