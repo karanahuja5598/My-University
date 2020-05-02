6d416a37721c3649d91b5b444b2b5b1aa5046630
agrashish <agrawalaashish1@gmail.com>
2020-05-01 23:07:26 -0500

    fixed test cases

--- app/test_app.py
+++ app/test_app.py
@@ -40,43 +40,43 @@ class FlaskAppTests(unittest.TestCase):
         self.driver.quit()
     
     # check to see if the index page loads the correct message when we aren't logged in
-    def test_index_unauth(self):
+    async def test_index_unauth(self):
         result = self.app.get('/')
-        exists = result.data.find("Welcome to the UIC School Notifier, please log in or register.".encode()) != -1
+        exists = (await result).data.find("Welcome to the UIC School Notifier, please log in or register.".encode()) != -1
         self.assertEqual(exists, True)
     
     # check to see if the login page loads the correct message when we aren't logged in
-    def test_login_unauth(self):
+    async def test_login_unauth(self):
         result = self.app.get('/login')
-        exists = result.data.find("Sign In".encode()) != -1
+        exists = (await result).data.find("Sign In".encode()) != -1
         self.assertEqual(exists, True)
 
     # check to see if the register page loads the correct message when we aren't logged in
-    def test_register_unauth(self):
+    async def test_register_unauth(self):
         result = self.app.get('/register')
-        exists = result.data.find("Register".encode()) != -1
+        exists = (await result).data.find("Register".encode()) != -1
         self.assertEqual(exists, True)
 
     # check to see if we can register properly
-    def test_register_auth(self):
+    async def test_register_auth(self):
         result = self.app.post('/register', data = {"username" : "m", "password" : "agfgggg"}, follow_redirects=True)
-        exists = result.data.find("Sign In".encode()) != -1
+        exists = (await result).data.find("Sign In".encode()) != -1
         self.assertEqual(exists, True)
 
     #check to see if we can register, then log in properly
-    def test_register_login_auth(self):
+    async def test_register_login_auth(self):
         result = self.app.post('/register', data = {"username" : "l", "password" : "agfgggg"}, follow_redirects=True)
-        exists = result.data.find("Sign In".encode()) != -1
+        await result
         result = self.app.post('/login', data = {"username" : "l", "password" : "agfgggg"}, follow_redirects=True)
-        exists = result.data.find("Welcome to the UIC School Notifier, l".encode()) != -1
+        exists = (await result).data.find("Welcome to the UIC School Notifier, l".encode()) != -1
         self.assertEqual(exists, True)
 
     # check to make sure that you can't register the same username twice
-    def test_double_register_auth(self):
+    async def test_double_register_auth(self):
         result = self.app.post('/register', data = {"username" : "x", "password" : "agfgghhh"}, follow_redirects=True)
-        exists = result.data.find("Sign In".encode()) != -1
+        await result
         result = self.app.post('/register', data = {"username" : "x", "password" : "agfgghhh"}, follow_redirects=True)
-        exists = result.data.find("Username already exists".encode()) != -1
+        exists = (await result).data.find("Username already exists".encode()) != -1
         self.assertEqual(exists, True)
 
 
